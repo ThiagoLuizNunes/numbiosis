c3a0df2603b228988131c551556f3287e4db213b
Vinícius Matheus <vinicius.matheus252@gmail.com>
2018-04-21 00:15:52 -0300

    Adiciona verificação de matrizes e separação de matrizes LU

--- js/matrixHelpers.js
+++ js/matrixHelpers.js
@@ -39,6 +39,43 @@ function pivoting(matrix, column) {
   console.log(matrix);
 }
 
+function isNullMatrix(matrix) {
+  let nullMatrix = true;
+  matrix.forEach(element => {
+    element.forEach(cell => {
+      if (cell != 0) {
+        nullMatrix = false;
+      }
+    })
+  })
+
+  return nullMatrix;
+}
+
+function sylvester(matrix) {
+  let auxMatrix = [];
+  let actualMatrix = [];
+  let det = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    actualMatrix = []
+    for (let j = 0; j <= i; j++) {
+      auxMatrix = [];
+      for (let k = 0; k <= i; k++) {
+        auxMatrix.push(matrix[j][k]);
+      }
+      actualMatrix.push(auxMatrix);
+    }
+
+    det = math.det(actualMatrix);
+    if (det <= 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function getTableValues() {
   /*
       Pegar os valores dos inputs da matriz de entrada
@@ -98,4 +135,43 @@ function createMatrixInput(rows, columns) {
 
 function hideMatrixInput() {
   document.getElementById("matrix").innerHTML = "";
+}
+
+function getLUMatrix(matrix, type, iteration = 0, label = '') {
+  let lu = [];
+  if (label === '') {
+    label = type;
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    let aux = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (type === "a") {
+        if (j < i) {
+          aux.push(underIndex("l", i + 1 + "" + (j + 1)));
+        } else {
+          aux.push(underIndex("u", i + 1 + "" + (j + 1)));
+        }
+      } else if (type === "l" && j <= i) {
+        if (j <= iteration && j < i) {
+          aux.push(matrix[i][j]);
+        } else if (j == i) {
+          aux.push(1);
+        } else {
+          aux.push(underIndex(label, i + 1 + "" + (j + 1)));
+        }
+      } else if (type === "u" && j >= i) {
+        if (i <= iteration) {
+          aux.push(matrix[i][j]);
+        } else {
+          aux.push(underIndex(label, i + 1 + "" + (j + 1)));
+        }
+      } else {
+        aux.push("0");
+      }
+    }
+    lu.push(aux);
+  }
+
+  return lu;
 }
